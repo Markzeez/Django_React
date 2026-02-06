025469c88061ad644c9907b846dcbf4e27d0b074
Azeez Ibrahim <139223601+Markzeez@users.noreply.github.com>
2026-02-06 15:57:01 +0100

    first

---
+++ backend/backend/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('create-user/', CreateUserView.as_view(), name='create-user'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+]
